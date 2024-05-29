ce52c1ce6332b40850714c28d102f8e2b17305d1
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-05-29 11:51:05 -0300

    feat: change icon on click

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 let acc = document.getElementsByClassName("accordion");
-let i;
-
-for (i = 0; i < acc.length; i++) {
+let icon = document.getElementsByClassName("iconButton");
+for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
     let panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
+      icon[i].src = './assets/images/icon-plus.svg';
     } else {
       panel.style.display = "block";
+      icon[i].src = './assets/images/icon-minus.svg';
     }
   });
 }
